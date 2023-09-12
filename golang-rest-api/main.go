4135146c83d66e12212008e0f26f9a457f902ec1
Fahlevi20 <rozakalfi@gmail.com>
2023-09-12 19:57:25 +0700

    belajar pake handler

--- golang-rest-api/main.go
+++ golang-rest-api/main.go
@@ -9,18 +9,23 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "Mukhammad Fahlevi Ali",
-			"bio":  "A software Engineer",
-		})
-	})
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"content": "Belajar golang",
-			"bio":     "belajar golang bareng levi gaskeun!!",
-		})
-	})
+	router.GET("/", RootHandler)
+
+	router.GET("/hello", HelloRootHandler)
 
 	router.Run()
+}
+
+func RootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Mukhammad Fahlevi Ali",
+		"bio":  "A software Engineer",
+	})
+}
+
+func HelloRootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"content": "Belajar golang",
+		"bio":     "belajar golang bareng levi gaskeun!!",
+	})
 }
